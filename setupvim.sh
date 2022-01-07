487076dfbfdc09ca098a84ff8652194639e7faa7
FabianAster <79381402+FabianAster@users.noreply.github.com>
2022-01-07 13:35:56 +0100

    Update setupvim.sh

--- setupvim.sh
+++ setupvim.sh
@@ -12,6 +12,7 @@ echo node js Installieren
 sudo npm install n -g
 sudo n stable
 
+sudo apt-get install python-dev python-pip python3-dev python3-pip
 
 echo Nvim Installieren
 sudo add-apt-repository ppa:neovim-ppa/stable
