62a8300aa455808ef3f51d50ef34730bd31323b1
FabianAster <fabast01@gmail.com>
2025-01-26 16:32:50 +0100

    updated harpoon ocnfigs

--- lua/plugins/file_manager.lua
+++ lua/plugins/file_manager.lua
@@ -1,27 +1,43 @@
 local function my_on_attach(bufnr)
-  local api = require("nvim-tree.api")
+    local api = require("nvim-tree.api")
 
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
 
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
 
-  -- custom mappings
-  vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Help"))
-  vim.keymap.set("n", "l", api.node.open.edit, opts("Help"))
-  vim.keymap.set("n", "n", api.fs.create, opts("Help"))
+    -- custom mappings
+    vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Help"))
+    vim.keymap.set("n", "l", api.node.open.edit, opts("Help"))
+    vim.keymap.set("n", "n", api.fs.create, opts("Help"))
+
+    -- Close the file tree when a file is selected
+    vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+        pattern = "*",
+        callback = function()
+            if vim.bo.filetype ~= "NvimTree" then
+                require("nvim-tree.api").tree.close()
+            end
+        end,
+    })
 end
 
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup({ on_attach = my_on_attach })
-  end,
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("nvim-tree").setup({
+            on_attach = my_on_attach,
+            view = {
+                width = 50, -- Set the default width here
+            },
+        })
+    end,
 }
