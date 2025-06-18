04e4fe6e7ba28a2c2c1b05eac81009140783577d
FabianAster <fabast01@gmail.com>
2025-06-18 16:34:32 +0200

    added error listing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -92,3 +92,5 @@ vim.api.nvim_set_keymap("v", "<leader>e", ":<C-u>MoltenEvaluateVisual<CR>gv", {}
 vim.api.nvim_set_keymap("n", "<leader>cg", ":lua vim.lsp.buf.hover()<CR>", {})
 
 vim.api.nvim_set_keymap("n", "<leader>ct", ":TodoTelescope<CR>", {})
+
+vim.api.nvim_set_keymap("n", "<leader>ce", ":Telescope diagnostics<CR>", {})
